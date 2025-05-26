1a0de0e1c395ab22fc812c70adb568b8574cf2be
Saksham Kumar <sakshamkumar03218@gmail.com>
2025-05-26 13:55:58 +0530

    cors

--- server/index.js
+++ server/index.js
@@ -21,6 +21,8 @@ app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
 app.use(cors(
     {
         origin: ["http://localhost:4000", "http://localhost:3000"],
+        methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+        allowedHeaders: ['Content-Type', 'Authorization'],
         credentials: true
     }
 ))
