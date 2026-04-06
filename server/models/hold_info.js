457c7e9ba19081c5db01250dfca41830708f6907
Saksham Kumar <sakshamkumar03218@gmail.com>
2026-04-06 10:54:19 +0530

    new data model

--- server/models/hold_info.js
+++ server/models/hold_info.js
@@ -1,14 +1,20 @@
 import mongoose from 'mongoose';
 
 const Hold_Info = new mongoose.Schema({
-    is_hold: { type: Boolean, default: false },
+    is_hold: {
+        type: Boolean,
+        default: false
+    },
     price: Number,
     partial_payment: Number,
     underprocessing_value: Number,
     remarks_from_proprietor: String,
     deduction_from_manager: Number,
     remarks_from_manager: String,
-    is_adhoc: Boolean,
+    is_adhoc: {
+        type: Boolean,
+        default: false
+    },
     hold_date: Date,
     put_on_hold_by: {
         type: String,
@@ -16,6 +22,10 @@ const Hold_Info = new mongoose.Schema({
     },
     holding_remarks: String,
     submit_to_proprietor_date: Date,
+    was_to_hold: {
+        type: Boolean,
+        default: false
+    },
     worker: {
         type: mongoose.Schema.Types.ObjectId,
         ref: 'Worker'
